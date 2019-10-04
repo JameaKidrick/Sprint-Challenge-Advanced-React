38876b6007113351c1767ae1e5647f6b0fee1fdc
JameaKidrick <50430635+JameaKidrick@users.noreply.github.com>
2019-10-04 19:04:36 -0400

    added extra test

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import App from './App';
-
+import PlayerCard from './components/PlayerCards'
 import { render } from '@testing-library/react'
+import PlayerCards from './components/PlayerCards';
 
 // INTEGRATION TEST FOR APP COMPONENT
 // test('renders without crashing', () => {
@@ -17,4 +18,11 @@ test('test to make sure the title is being displayed on DOM', () => {
 test('test to make sure the title is being displayed on DOM', () => {
   const { getByPlaceholderText } = render (<App />)
   getByPlaceholderText(/Favorite player/i)
+})
+
+test('test to make sure individual items are displayed properly', () => {
+  const Name = 'name'
+  const Country = 'country'
+  const PlayerCard = render(<PlayerCards name={Name} country={Country} />)
+  PlayerCard.getByText(`${Name} playing for ${Country}`)
 })
--- client/src/components/PlayerCards.js
+++ client/src/components/PlayerCards.js
@@ -1,5 +1,7 @@
 import React from 'react'
 
+
+
 const PlayerCards = props => {
   return(
     <div>
